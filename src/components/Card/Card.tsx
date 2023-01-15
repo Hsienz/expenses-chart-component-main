44af07d3429b8f7f69dbae2f32eb9381028ee9a5
Hsienz <55347238+Hsienz@users.noreply.github.com>
2023-01-16 06:43:24 +0800

    first commit

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,11 @@
+import Chart from "../Chart/Chart"
+import Balance from "../Balance/Balance"
+const Card = () => {
+    return (
+        <div>
+            <Balance />
+            <Chart />
+        </div>
+    )
+}
+export default Card
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{tsx,ts,jsx,js}'],
+  theme: {
+    extend: {
+      colors: {
+
+      },
+      fontFamily: {
+        sans: ['DM Sans', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
